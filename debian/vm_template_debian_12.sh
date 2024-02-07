e068a782f4801e9a553ff391fca555e85ebded06
Mohamed Zarka <medzarka@gmail.com>
2024-02-07 14:32:24 +0300

    update debian vm template creation

--- debian/vm_template_debian_12.sh
+++ debian/vm_template_debian_12.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "-----------------------------------------------------------------"
-echo "Create a Ubuntu Linux 22.04 template VM"
+echo "Create a Debian Linux 12 template VM"
 echo "-----------------------------------------------------------------"
 
 # [x] Load common configs from the common scripts
@@ -10,18 +10,18 @@ source ../lib/common_vm_scripts.sh
 
 # [x] Specific VM template configurations
 # Specific configs
-TEMPLATE_VM_ID=5122
+TEMPLATE_VM_ID=5212
 TEMPLATE_TYPE=template
-TEMPLATE_OS=ubuntu
-TEMPLATE_VERSION=22.04
+TEMPLATE_OS=debian
+TEMPLATE_VERSION=12
 RAM=1024
 CORES=1
-IP=192.168.50.11/24
+IP=192.168.50.22/24
 # RECHECK check the size
 DISKIMAGE_SIZE=4 
 
-IMAGE_URL=https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-IMAGE_NAME=jammy-server-cloudimg-amd64.img
+IMAGE_URL=https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.raw
+IMAGE_NAME=debian-12-generic-amd64.raw
 IMAGE_PATH=/var/lib/vz/template/cache/$IMAGE_NAME
 
 # [x] Load common VM template configurations
@@ -39,8 +39,8 @@ create_retrive_specific_template_user_password_from_pass $TEMPLATE_VM_ID $DEFAUL
 USER_PASSWORD=$RETURN_VALUE
 
 TEMPLATE_NAME=${TEMPLATE_TYPE}-${TEMPLATE_OS}-${TEMPLATE_VERSION} 
-STORAGE=local-lvm  
-GW=192.168.50.1 
+STORAGE=local-lvm   
+GW=192.168.50.1
 DNS=192.168.50.1
 VLAN=50
 BRIDGE=vmbr1 
@@ -61,7 +61,31 @@ create_new_template
 
 #############################################################
 echo "----------------------------------------------------------------------------------------"
-echo "Install Ubuntu 22.04 VM template ..."
+echo "Install Debian 12 VM template ..."
 echo "----------------------------------------------------------------------------------------"
 echo "Post-task: login to the ubuntu user, copy the setup file, and run it."
 echo "----------------------------------------------------------------------------------------"
+
+
+#sudo apt install vim qemu-guest-agent iputils-ping git -y
+
+#cloud-init clean
+
+#tasksel --list-tasks
+
+#sudo truncate -s 0 /etc/machine-id
+
+
+#vi /etc/netplan/01-netcfg.yaml
+#network:
+#  version: 2
+#  renderer: networkd
+#  ethernets:
+#    ens18:
+#      dhcp4: no
+#      addresses: [128.204.192.xxx/24]
+#      gateway4: 128.204.192.1
+#      nameservers:
+#        addresses: [89.207.128.252,89.207.130.252]
+#      dhcp6: no```
+#netplan apply
